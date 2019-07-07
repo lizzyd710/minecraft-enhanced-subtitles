75726287a317c0f2ad7e98e6cd18e2f1ef8263bd
Elizabeth Sherrock <lizzyd710@gmail.com>
2019-07-07 12:16:57 -0400

    Filled out some basic starting code for the event handler.

--- src/main/java/lizzyd710/movablesubtitles/OverlayEventHandler.java
+++ src/main/java/lizzyd710/movablesubtitles/OverlayEventHandler.java
@@ -1,5 +1,22 @@
 package lizzyd710.movablesubtitles;
 
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@EventBusSubscriber
 public class OverlayEventHandler {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    @SubscribeEvent(receiveCanceled = true)
+    public void onEvent(RenderGameOverlayEvent.Pre event) {
+        if(event.getType() == RenderGameOverlayEvent.ElementType.SUBTITLES) {
+            // this is where the magic happens
+            LOGGER.info("SUBTITLE PRERENDER EVENT");
+        }
+    }
 
 }
