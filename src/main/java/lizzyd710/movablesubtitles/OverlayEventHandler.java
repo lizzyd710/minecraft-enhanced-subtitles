45768e7dc617223e652ce58a213850cf6527af57
Elizabeth Sherrock <lizzyd710@gmail.com>
2020-04-25 16:41:12 -0400

    Renamed some variables for clarity

--- src/main/java/lizzyd710/movablesubtitles/OverlayEventHandler.java
+++ src/main/java/lizzyd710/movablesubtitles/OverlayEventHandler.java
@@ -1,7 +1,5 @@
 package lizzyd710.movablesubtitles;
 
-import java.util.Iterator;
-import java.util.List;
 import com.google.common.collect.Lists;
 import com.mojang.blaze3d.platform.GlStateManager;
 import lizzyd710.movablesubtitles.config.MovableSubtitlesConfig;
@@ -9,6 +7,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.ISoundEventListener;
 import net.minecraft.client.audio.SoundEventAccessor;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.overlay.SubtitleOverlayGui;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.MathHelper;
@@ -16,7 +15,9 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraft.client.gui.AbstractGui;
+
+import java.util.Iterator;
+import java.util.List;
 
 @EventBusSubscriber
 public class OverlayEventHandler implements ISoundEventListener {
@@ -55,7 +56,6 @@ public class OverlayEventHandler implements ISoundEventListener {
             Vec3d vec3d2 = (new Vec3d(0.0D, 1.0D, 0.0D)).rotatePitch(-mc.player.rotationPitch *
                     ((float) Math.PI / 180F)).rotateYaw(-mc.player.rotationYaw * ((float) Math.PI / 180F));
             Vec3d vec3d3 = vec3d1.crossProduct(vec3d2);
-            int i = 0;
             int maxLength = 0;
             Iterator<SubtitleOverlayGui.Subtitle> iterator = this.subtitles.iterator();
 
@@ -71,35 +71,36 @@ public class OverlayEventHandler implements ISoundEventListener {
             maxLength = maxLength + mc.fontRenderer.getStringWidth("<") + mc.fontRenderer.getStringWidth(" ")
                     + mc.fontRenderer.getStringWidth(">") + mc.fontRenderer.getStringWidth(" ");
 
+            int captionIndex = 0;
             for (SubtitleOverlayGui.Subtitle caption1 : this.subtitles) {
                 String captionText = caption1.getString();
-                Vec3d distanceToPlayer = caption1.getLocation().subtract(playerPosVec).normalize();
-                double d0 = -vec3d3.dotProduct(distanceToPlayer);
-                double d1 = -vec3d1.dotProduct(distanceToPlayer);
+                Vec3d soundDistanceToPlayer = caption1.getLocation().subtract(playerPosVec).normalize();
+                double d0 = -vec3d3.dotProduct(soundDistanceToPlayer);
+                double d1 = -vec3d1.dotProduct(soundDistanceToPlayer);
                 boolean flag = d1 > 0.5D;
-                int l = maxLength / 2;
+                int halfMaxLength = maxLength / 2;
                 int subtitleWidth = mc.fontRenderer.getStringWidth(captionText);
                 int l1 = MathHelper.floor(MathHelper.clampedLerp(255.0D, 75.0D,
                         (double) ((float) (Util.milliTime() - caption1.getStartTime()) / 3000.0F)));
                 int textColor = l1 << 16 | l1 << 8 | l1;
                 GlStateManager.pushMatrix();
-                GlStateManager.translatef((float) mc.mainWindow.getScaledWidth() - (float) l * 1.0F - GLTRANSLATE_X,
-                        (float) (mc.mainWindow.getScaledHeight() - GLTRANSLATE_Y1) - (float) (i * 10) * 1.0F,
+                GlStateManager.translatef((float) mc.mainWindow.getScaledWidth() - (float) halfMaxLength * 1.0F - GLTRANSLATE_X,
+                        (float) (mc.mainWindow.getScaledHeight() - GLTRANSLATE_Y1) - (float) (captionIndex * 10) * 1.0F,
                         0.0F);
                 GlStateManager.scalef(1.0F, 1.0F, 1.0F); // might be unnecessary
-                AbstractGui.fill(-l - 1, -5, l + 1,
+                AbstractGui.fill(-halfMaxLength - 1, -5, halfMaxLength + 1,
                         5, mc.gameSettings.func_216841_b(0.8F));
                 GlStateManager.enableBlend();
                 if (!flag) {
                     if (d0 > 0.0D)
-                        mc.fontRenderer.drawString(">", (float) (l - mc.fontRenderer.getStringWidth(">")),
+                        mc.fontRenderer.drawString(">", (float) (halfMaxLength - mc.fontRenderer.getStringWidth(">")),
                                 -4.0F, textColor - 16777216);
-                    else if (d0 < 0.0D) 
-                        mc.fontRenderer.drawString("<", (float) (-l), -4.0F, textColor - 16777216);
+                    else if (d0 < 0.0D)
+                        mc.fontRenderer.drawString("<", (float) (-halfMaxLength), -4.0F, textColor - 16777216);
                 }
                 mc.fontRenderer.drawString(captionText, (float) (-subtitleWidth / 2), -4.0F, textColor - 16777216);
                 GlStateManager.popMatrix();
-                i++;
+                captionIndex++;
             }
             GlStateManager.disableBlend();
             GlStateManager.popMatrix();
@@ -109,17 +110,17 @@ public class OverlayEventHandler implements ISoundEventListener {
     @Override
     public void onPlaySound(ISound soundIn, SoundEventAccessor accessor) {
         if (accessor.getSubtitle() != null) {
-            String s = accessor.getSubtitle().getFormattedText();
+            String subtitleText = accessor.getSubtitle().getFormattedText();
             if (!this.subtitles.isEmpty()) {
                 for (SubtitleOverlayGui.Subtitle caption : this.subtitles) {
-                    if (caption.getString().equals(s)) {
+                    if (caption.getString().equals(subtitleText)) {
                         caption.refresh(new Vec3d((double) soundIn.getX(), (double) soundIn.getY(), (double) soundIn.getZ()));
                         return;
                     }
                 }
             }
             this.subtitles.add(new SubtitleOverlayGui(Minecraft.getInstance()).
-                    new Subtitle(s, new Vec3d((double) soundIn.getX(), (double) soundIn.getY(), (double) soundIn.getZ())));
+                    new Subtitle(subtitleText, new Vec3d((double) soundIn.getX(), (double) soundIn.getY(), (double) soundIn.getZ())));
         }
     }
 }
