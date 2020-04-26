af9ac5d520c7f77d00bf37f104bcd8c252888b30
Elizabeth Sherrock <lizzyd710@gmail.com>
2020-04-26 14:15:03 -0400

    Removed unnecessary methods and fields in the enum

--- src/main/java/lizzyd710/movablesubtitles/OverlayPosition.java
+++ src/main/java/lizzyd710/movablesubtitles/OverlayPosition.java
@@ -6,50 +6,12 @@ package lizzyd710.movablesubtitles;
  * @author Elizabeth Sherrock
  */
 public enum OverlayPosition {
-    BOTTOM_RIGHT(0, 2.0F, 30),
-    //TODO: Figure out real X and Y.
-    BOTTOM_CENTER(1, 2.0F, 30),
-    BOTTOM_LEFT(2, 2.0F, 30),
-    CENTER_LEFT(3, 2.0F, 30),
-    TOP_LEFT(4, 2.0F, 30),
-    TOP_CENTER(5, 2.0F, 30),
-    TOP_RIGHT(6, 2.0F, 30),
-    CENTER_RIGHT(7, 2.0F, 30);
-
-    private final int id;
-    private final float xOffset;
-    private final int yOffset;
-
-    OverlayPosition(int i, float x, int y) {
-        id = i;
-        xOffset = x;
-        yOffset = y;
-    }
-
-    /**
-     * Returns the numerical id of the enum.
-     *
-     * @return the numerical id of the enum
-     */
-    public int getId() {
-        return this.id;
-    }
-
-    /**
-     * Returns the x value used to transform the overlay into the enum's position
-     *
-     * @return the x value used to transform the overlay into the enum's position
-     */
-    public float getXOffset() {
-        return this.xOffset;
-    }
-
-    /**
-     * Returns the y value used to transform the overlay into the enum's position
-     *
-     * @return the y value used to transform the overlay into the enum's position
-     */
-    public int getYOffset() {
-        return this.yOffset;
-    }
+    BOTTOM_RIGHT,
+    BOTTOM_CENTER,
+    BOTTOM_LEFT,
+    CENTER_LEFT,
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    CENTER_RIGHT;
 }
