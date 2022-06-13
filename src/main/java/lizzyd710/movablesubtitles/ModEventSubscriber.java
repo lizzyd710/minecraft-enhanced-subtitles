8b3086e06b2fc1aefb40a4248c8778504f3d9f37
Elizabeth Sherrock <lizzyd710@gmail.com>
2022-06-12 20:17:04 -0400

    Updating for new versions (#4)
    * Updated gradle.
    
    * Updated code to work with the new versions of Forge and Minecraft

--- src/main/java/lizzyd710/movablesubtitles/ModEventSubscriber.java
+++ src/main/java/lizzyd710/movablesubtitles/ModEventSubscriber.java
@@ -4,11 +4,12 @@ import lizzyd710.movablesubtitles.config.ConfigHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = "movablesubtitles", bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ModEventSubscriber {
     @SubscribeEvent
-    public static void onModConfigEvent(final ModConfig.ModConfigEvent event) {
+    public static void onModConfigEvent(final ModConfigEvent event) {
         final ModConfig config = event.getConfig();
         ConfigHelper.bakeClient(config);
     }
